5053cc60356314b1a2116416f0c03914fcaa9a43
Alan <alantronchi@ceroca.cat>
2024-10-24 15:38:35 +0200

    pantalla principal

---
+++ Assets/Scripts/PrimeraPantalla.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PrimeraPantalla : MonoBehaviour
+{
+
+    public void AnarAJugar()
+    {
+        SceneManager.LoadScene("Jugar");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
