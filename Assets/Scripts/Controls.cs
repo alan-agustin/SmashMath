5e3bef1a3669a643003bc8575e857f233c7095a6
Alan <alantronchi@ceroca.cat>
2024-10-24 16:42:51 +0200

    modificacio pantalla controls

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Controls : MonoBehaviour
+{
+
+    public void AnarAControls()
+    {
+        SceneManager.LoadScene("Controles");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TornarInici.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TornarInici : MonoBehaviour
+{
+
+    public void AnarAlMenu()
+    {
+        SceneManager.LoadScene("Principal");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
